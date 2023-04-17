0ab7447ce454f92f1ea1c53f43fda045554ae22e
carlos-mercado <94933155+carlos-mercado@users.noreply.github.com>
2023-04-16 22:25:10 -0700

    Update montyHall.py

--- montyHall.py
+++ montyHall.py
@@ -4,111 +4,137 @@
     #(i) randomly switch to one of the remaining doors or
     #(ii) stick with your original selection
 import random
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
 
-class DoorsTrial:
-    def __init__(self, nDoors):
-        self.nDoors = nDoors
+def montyFall(nDoors, trials, stayOrChange, actualTrials):
+    wins = 0
+    for i in range(trials):
+        #creating the "stage"
+        doors = ["GOAT"] * nDoors
+        carDoor = random.randint(0, nDoors - 1)
+        doors[carDoor] = "CAR"
 
-    def keepDoor(self):
-        wins = 0
-        bananaWins = 0
-        trials = 100000
+        #contestant makes their choice
+        contestantChoice = random.randint(0, nDoors - 1)
 
-        for i in range(trials):
-            #creating the "stage"
-            doors = ["GOAT"] * self.nDoors
-            carDoor = random.randint(0, self.nDoors - 1)
-            doors[carDoor] = "CAR"
+        #host slips and opens a door that could be the car door
+        for j in range(nDoors):
+            if j != contestantChoice:
+                deletedDoor = j
+                doors[j] = "X"
+                break
+        
+        #if the host accidentally opens the car door he gets fired 
+        #and the game does not count (so dont count it as a trial)
+        if deletedDoor == carDoor:
+            continue
 
-            #contestant makes their choice
-            contestantDoorChoice = random.randint(0, self.nDoors - 1)
+        actualTrials+=1
+        #now contestant makes a choice. Stay with the originally picked door or choose a new one
+        #choice = random.randint(0, 1)
+        choice = stayOrChange;
 
-            wins += 1 if doors[contestantDoorChoice] == "CAR" else 0
+        if choice == 0: #stay
+            result = doors[contestantChoice];
+        if choice == 1: #newdoor
+            del doors[contestantChoice]
+            doors.remove("X")
+            result = doors[random.randint(0, len(doors) - 1)]
 
-        percentageWins = (wins / trials) * 100
-        #calculating banana case wins
-        bananaPercentageWins = (bananaWins / trials) * 100
-        print(str(percentageWins) + "% of trials are wins if you stay with " + str(self.nDoors) + " doors possible. ")
-        #printing banana case wins
-        print("For " + str(self.nDoors) + " trials where host slips on banana " + str(bananaPercentageWins) + "% of trials are wins.\n")
+        wins += 1 if result == "CAR" else 0
+    return (actualTrials, wins)
 
-    def switchDoor(self):
-        wins = 0
-        bananaWins = 0
-        trials = 100000
+def monty(nDoors, trials, stayOrChange):
+    wins=0
+    for i in range(trials):
+        #creating the "stage"
+        doors = ["GOAT"] * nDoors
+        carDoor = random.randint(0, nDoors - 1)
+        doors[carDoor] = "CAR"
 
-        for i in range(trials):
-            #creating the "stage"
-            doors = ["GOAT"] * self.nDoors
-            carDoor = random.randint(0, self.nDoors - 1)
-            doors[carDoor] = "CAR"
+        #contestant makes their choice
+        contestantChoice = random.randint(0, nDoors - 1)
 
-            #contestant makes their choice
-            contestantDoorChoice = random.randint(0, self.nDoors - 1)
-
-            wins += 1 if doors[contestantDoorChoice] != "CAR" else 0
-
-        percentageWins = (wins / trials) * 100
-        #calculating banana case wins
-        bananaPercentageWins = (bananaWins / trials) * 100
-        print(str(percentageWins) + "% of trials are wins if you always switch. ")
-        #printing banana case wins
-        print("For " + str(self.nDoors) + " trials where host slips on banana " + str(bananaPercentageWins) + "% of trials are wins.\n")
-
-    def randomPick(self):
-        wins = 0
-        bananaWins = 0
-        trials = 100000
-
-        for i in range(trials):
-            #creating the "stage"
-            doors = ["GOAT"] * self.nDoors
-            carDoor = random.randint(0, self.nDoors - 1)
-            doors[carDoor] = "CAR"
-
-            #contestant makes their choice
-            contestantDoorChoice = random.randint(0, self.nDoors - 1)
-
-            if random.randint(0, 1) == 0: #stay with original door
-                wins += 1 if doors[contestantDoorChoice] == "CAR" else 0
-            else: #choose a new door
-                wins += 1 if doors[contestantDoorChoice] != "CAR" else 0
-
-        percentageWins = (wins / trials) * 100
-        #calculating banana case wins
-        bananaPercentageWins = (bananaWins / trials) * 100
-        print(str(percentageWins) + "% of trials are wins if randomly stay and or switch. ")
-        #printing banana case wins
-        print("For " + str(self.nDoors) + " trials where host slips on banana " + str(bananaPercentageWins) + "% of trials are wins.\n")
+        #host takes away one door (that the contestant didnt pick and does not have the car)
+        for j in range(nDoors):
+            if doors[j] == "GOAT" and j != contestantChoice:
+                doors[j] = "X"
+                break
 
 
+        #now contestant makes a choice. Stay with the originally picked door or choose a new one
+        #choice = random.randint(0, 1)
+        choice = stayOrChange;
 
-trialOf3 = DoorsTrial(3)
-trialOf3.keepDoor()
-trialOf3.switchDoor()
-trialOf3.randomPick()
-print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n")
+        if choice == 0: #stay
+            result = doors[contestantChoice];
+        if choice == 1: #newdoor
+            del doors[contestantChoice]
+            doors.remove("X")
+            result = doors[random.randint(0, len(doors) - 1)]
 
-trialOf6 = DoorsTrial(6)
-trialOf6.keepDoor()
-trialOf6.switchDoor()
-trialOf6.randomPick()
-print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n")
+        wins += 1 if result == "CAR" else 0
+    return wins
 
-trialOf9 = DoorsTrial(9)
-trialOf9.keepDoor()
-trialOf9.switchDoor()
-trialOf9.randomPick()
-print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n")
+def plotApproximationImprovement(nDoors, stayOrChange, actualProb):
+    results = {}
+    trials = 5
 
-trialOf20 = DoorsTrial(20)
-trialOf20.keepDoor()
-trialOf20.switchDoor()
-trialOf20.randomPick()
-print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n")
+    while(trials < 500000):
+        results[trials] = monty(nDoors, trials, stayOrChange) / trials
+        trials*=5
 
-trialOf100 = DoorsTrial(100)
-trialOf100.keepDoor()
-trialOf100.switchDoor()
-trialOf100.randomPick()
-print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n")
+
+    # calculate the slope and y-intercept of the line
+    x = [0, len(results)]
+    y = [actualProb, actualProb]
+    m, b = np.polyfit(x, y, 1)
+
+    i = 0
+    for (key, value) in results.items():
+        plt.scatter(i, value)
+        
+        # calculate the distance from the point to the line
+        x_point = i
+        y_point = value
+        distance = abs(m * x_point - y_point + b) / np.sqrt(m**2 + 1)
+        
+        # draw a vertical line segment to represent the gap
+        plt.plot([i, i], [value, actualProb], linestyle='--', color='gray', linewidth=1)
+        
+        i += 1
+
+    # draw the line
+
+    plt.axhline(y=actualProb, color='r', linestyle='-')
+    plt.xlabel('5^n Trials')
+    plt.ylabel('Win Rate')
+    plt.title(str(nDoors) + ' Doors case ' + str(stayOrChange))
+    plt.show()
+
+def plotProbability(nDoors):
+    xLabels = ["Stay Variant", "Switch Variant"]
+    values = [monty(nDoors, 100000, 0)/100000,monty(nDoors, 100000, 1)/100000]
+
+    plt.bar(xLabels[0], values[0], color="blue")
+    plt.bar(xLabels[1], values[1], color="red")
+    plt.xlabel('Cases')
+    plt.ylabel('Win Rate')
+    plt.title(str(nDoors) + " Doors")
+    plt.show()
+
+def plotProbabilityMontyFall(nDoors):
+    xLabels = ["Stay Variant", "Switch Variant"]
+    (trials1, wins1) = montyFall(nDoors, 100000, 0, 0)
+    (trials2, wins2) = montyFall(nDoors, 100000, 1, 0)
+    values = [wins1/trials1, wins2/trials2]
+
+    plt.bar(xLabels[0], values[0], color="blue")
+    plt.bar(xLabels[1], values[1], color="red")
+    plt.xlabel('Cases')
+    plt.ylabel('Win Rate')
+    plt.title(str(nDoors) + " Doors")
+    plt.show()
+
